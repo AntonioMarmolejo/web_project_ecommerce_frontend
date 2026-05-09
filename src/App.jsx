7e8b68c418d219a4d641943c98a71461f4341527
Antonio Marmolejo <djcamg71@gmail.com>
2026-05-09 00:40:50 -0500

    feat: se agregan las rutas de paginación en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
-import './styles/App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Páginas
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
 
 function App() {
     return (
-        <>
-            <h1>Vite + React</h1>
-        </>
-    )
+        <BrowserRouter>
+            <Routes>
+
+                {/* Rutas públicas */}
+                <Route path="/" element={<Home />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/products/:id" element={<ProductDetail />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+
+                {/* Rutas protegidas (por ahora públicas, JWT en Sprint 3) */}
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/favorites" element={<Favorites />} />
+
+                {/* 404 */}
+                <Route path="*" element={<NotFound />} />
+
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
